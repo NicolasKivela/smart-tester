8277d2cf9bad1c856d7175c8bc69a732a09bce76
Susanna Peltoniemi <susanna.peltoniemi@tuni.fi>
2025-09-26 15:30:18 +0300

    feat: initialize result components on front end

---
+++ frontend/src/components/ResultView/BddView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="bdd-view">
+        <div class="column">
+            <h3 class="title">BDD Scenarios</h3>
+            <button class="primary">Generate Tests</button>
+        </div>
+        <div class="code-block">
+            BDD Scenarios
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.bdd-view {
+    border: 2px solid #ccc;
+    padding: 1rem;
+    border-radius: 4px;
+}
+.code-block {
+    background-color: black;
+    color: white;
+    height: 400px;
+    border-radius: 4px;
+    padding: 1rem;
+}
+.column {
+    display: flex;
+    gap: 1rem;
+}
+.title {
+    flex: 8;
+    justify-self: start;
+    align-self: center;
+    margin-top: 0.5rem;
+}
+button {
+    flex: 2;
+}
+</style>
---
+++ frontend/src/components/ResultView/CodeView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="code-view">
+        <div class="column">
+            <h3 class="title">Generated Code</h3>
+            <button class="secondary">Download .zip</button>
+            <button class="secondary">Copy</button>
+            <button class="primary">Reset Session</button>
+        </div>
+        <div class="code-block">
+            Generated code
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.code-view {
+    border: 2px solid #ccc;
+    padding: 1rem;
+    border-radius: 4px;
+}
+.code-block {
+    background-color: black;
+    color: white;
+    height: 400px;
+    border-radius: 4px;
+    padding: 1rem;
+}
+.column {
+    display: flex;
+    gap: 1rem;
+}
+.title {
+    flex: 6;
+    justify-self: start;
+    align-self: center;
+    margin-top: 0.5rem;
+}
+button {
+    flex: 3;
+}
+</style>
