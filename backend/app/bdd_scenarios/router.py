1604ded0fbb3cd8eed97d2fd05933e984fd09e08
Nicolas Kivelä <nicolas.kivela@gmail.com>
2025-10-09 10:25:48 +0300

    Fix: Changed endpoint titles

--- backend/app/bdd_scenarios/router.py
+++ backend/app/bdd_scenarios/router.py
@@ -12,13 +12,13 @@ async def update_bdd_scenario(item_id: int, item: BDD_Scenario):
     BDD_SCENARIOS[item.id] = item.model_dump()
     return {"message": "Item replaced", "id":item_id}
 
-@router.post("/bdd_scenarios/",tags=["bdd_scenarios"])
+@router.post("/bdd_scenarios",tags=["bdd_scenarios"])
 async def create_bdd_scenario(item: BDD_Scenario):
     BDD_SCENARIOS[item.id]=item.model_dump()
     return {"message": "Item created" , "id":item.id}
 
 #Generate BDD_scenarios
-@router.post("/bdd_scenarios",tags=["bdd_scenarios"])
+@router.post("/bdd_scenarios/generate",tags=["bdd_scenarios"])
 async def generate_bdd_scenarios(item: Generate_BDD):
     #TODO: Call async generator model
     #async generator(item)
