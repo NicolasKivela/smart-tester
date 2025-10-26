68e380b93efd2189379e8aaa9ecd8459d3531feb
Mikael Alamäki <mikael.alamaki@tuni.fi>
2025-10-26 19:14:43 +0200

    Intializing the main public method, still not working though

--- backend/app/locator_retrieval/Task_agent.py
+++ backend/app/locator_retrieval/Task_agent.py
@@ -10,7 +10,7 @@ class BDDTaskAgent(BaseAgent):
 
     You receive one or more BDD scenarios and a URL. 
     Your job is to plan a single efficient route through the website that satisfies ALL BDD scenarios 
-    with the minimum number of steps.
+    with the minimum number of steps. Clearly define the last step of the plan. Based on the last part the task list can be stated as finished
 
     Purpose of the plan is to make route to get all relevant locators for the bdd scenarios using the plan.
     
